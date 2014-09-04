349f5108c877a2759c73a693b3b28b780b2e928d
Rob Lamog <shiftf8@gmail.com>
2014-09-04 14:25:25 -0700

    start of triangles lab

---
+++ lab1bTriangles.c
@@ -0,0 +1,21 @@
+/*
+Lamog, Robert
+Lab 1B
+09/16/2014
+Testing sides and triangles.
+*/
+
+#include <math>
+#include <stdio.h>
+
+char toContinue;
+int side1=side2=side3=0;
+
+int main() {
+    do {
+        /* do stuff */
+    } while (toContinue != 'n');
+
+    getchar();
+    return 0;
+}
