bf6a3d07f3481dd147c658d963711120f79605c4
Rob Lamog <shiftf8@gmail.com>
2014-09-04 17:51:01 -0700

    untested build using single while loop, unfinished selection structures

--- lab1bTriangles.c
+++ lab1bTriangles.c
@@ -2,20 +2,34 @@
 Lamog, Robert
 Lab 1B
 09/16/2014
-Testing sides and triangles.
+Testing sides to make triangles.
 */
 
-#include <math>
+#include <math.h>
 #include <stdio.h>
 
-char toContinue;
-int side1=side2=side3=0;
-
 int main() {
-    do {
-        /* do stuff */
-    } while (toContinue != 'n');
+    long sidesArr[3] = {0};
+    char toContinue = NULL;
+    long input = 0;
+    int i = 0;
 
-    getchar();
+    while (input != 'N'){
+        printf("Please enter the length of a side of a triangle: ");
+        scanf("%ld", &input);
+        if (input > 0) {
+            sidesArr[i] = input;
+            i++;
+        }
+        else printf("Your input is invalid. Please enter an unsigned integer as the length of your side.");
+
+        if (i == 3) {
+            /* output test results, reset 'i' and ask to terminate program */
+            i = 0;
+            printf("Would you like to test another triangle? Type \'N\' to exit test.\n");
+            scanf("%c", &toContinue);
+        }
+    }
+
     return 0;
 }
