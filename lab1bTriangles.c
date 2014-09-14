ce74bfa8d19a4608aae36cd14b8cac27ed7cd184
Rob Lamog <shiftf8@gmail.com>
2014-09-13 23:49:42 -0700

    virtual studio messed up my spacing.

--- lab1bTriangles.c
+++ lab1bTriangles.c
@@ -18,99 +18,105 @@ void printTriangleArea( double area );
 void printTriangleType( int type );
 
 int main() {
-	triangleTest();
+    triangleTest();
 
-	return 0;
+    return 0;
 }
 
 void triangleTest() {
-	char response[512] = { 'y' };
+    /* Not checking for overflow. Not expecting user to input more than 512 characters. */
+    char response[512] = { 'y' };
 
-	printf("Welcome. Let's test some triangles, shall we?\n");
+    printf("Welcome. Let's test some triangles, shall we?\n");
 
-	while ((response[0] == 'y') || (response[0] == 'Y')) {
+    while ((response[0] == 'y') || (response[0] == 'Y')) {
         char c;
-		double triangleSidesArr[3] = { 0.0, 0.0, 0.0 };
+        double triangleSidesArr[3] = { 0.0, 0.0, 0.0 };
 
-		inputSides(triangleSidesArr, 3);
-		/*        printTriangleSides(triangleSidesArr);
-		printf("\n");   /**/
+        inputSides(triangleSidesArr, 3);
+/*        printTriangleSides(triangleSidesArr);
+        printf("\n");   /**/
 
-		if (triangleSidesCheck(triangleSidesArr) == 3) {
-			printf("Success! The lengths ");
-			printTriangleSides(triangleSidesArr);
-			printf(" can make a");
-			printTriangleType(triangleType(triangleSidesArr));
-			printf(" triangle with an approximate area of ");
-			printTriangleArea(triangleArea(triangleSidesArr));
-			printf(".\nWould you like to try again (Y/N)?\n");
-		}
-		else {
-			printf("It is NOT possible to make a triangle with sides of length ");
-			printTriangleSides(triangleSidesArr);
-			printf(".\nWould you like to try again (Y/N)?\n");
-		}
+        if (triangleSidesCheck(triangleSidesArr) == 3) {
+            printf("Success! The lengths ");
+            printTriangleSides(triangleSidesArr);
+            printf(" can make a");
+            printTriangleType(triangleType(triangleSidesArr));
+            printf(" triangle with an approximate area of ");
+            printTriangleArea(triangleArea(triangleSidesArr));
+            printf(".\nWould you like to try again (Y/N)?\n");
+        } else {
+            printf("It is NOT possible to make a triangle with sides of length ");
+            printTriangleSides(triangleSidesArr);
+            printf(".\nWould you like to try again (Y/N)?\n");
+        }
 		
-		while ((c = getchar()) != '\n' && c != EOF) {
-			/* trying to discard stdin stream */
-		}
-		scanf("%s", response);
-	}
-} /* main */
-
-void inputSides( double sidesArr[], int arrLength ) {
-	int i = 0;
-	double input = 0.0;
-
-	/* i is incrementing upon successful length input */
-	for (i = 0; i < arrLength;) {
-		int returnSuccessful = 0;
-		char c;
-
-		printf("Please enter a length.\n");
-		if (i > 0) {
-			while ((c = getchar()) != '\n' && c != EOF) {
-				/* trying to discard stdin stream */
-			}
-		}
-
-		returnSuccessful = scanf("%lf", &input);
-		/*        printf("%lf\n", input);    /**/
-
-		if (returnSuccessful == 1) {
-			if (input > 0) {
-				sidesArr[i] = input;
-				i++;
-			}
-			if (input <= 0) {
-				printf("Oops! A length must be positive. ");
-			}
-		}
-		if (returnSuccessful < 1) {
-			scanf("%*s");    /**/
-			printf("Invalid input. ");
-		}
-	}
+        while ((c = getchar()) != '\n' && c != EOF) {
+            /* trying to discard stdin stream */
+        }
+        scanf("%s", response);
+    }
 }
 
-/* A triangle is possible @return 3 */
+void inputSides( double sidesArr[], int arrLength ) {
+    int i = 0;
+    double input = 0.0;
+
+    /* i is incrementing upon successful length input */
+    for (i = 0; i < arrLength;) {
+        int returnSuccessful = 0;
+        char c;
+
+        printf("Please enter a numeric length.\n");
+        if (i > 0) {
+            while ((c = getchar()) != '\n' && c != EOF) {
+                /* trying to discard stdin stream */
+            }
+        }
+
+        returnSuccessful = scanf("%lf", &input);
+/*        printf("%lf\n", input);    /**/
+
+        if (returnSuccessful == 1) {
+            if (input > 0) {
+                sidesArr[i] = input;
+                i++;
+            }
+            if (input <= 0) {
+                printf("Oops! A length must be positive. ");
+            }
+        }
+        if (returnSuccessful < 1) {
+            while ((c = getchar()) != '\n' && c != EOF) {
+                /* trying to discard stdin stream */
+            }
+/*            scanf("%*s");    /**/
+            printf("Invalid input. ");
+        }
+    }
+}
+
+/*
+A triangle is possible @return 3
+*/
 int triangleSidesCheck( double arr[] ) {
-	int isPossible = 0;
+    int isPossible = 0;
 
-	if (arr[0] + arr[1] > arr[2]) isPossible++;
-	if (arr[1] + arr[2] > arr[0]) isPossible++;
-	if (arr[2] + arr[0] > arr[1]) isPossible++;
+    if (arr[0] + arr[1] > arr[2]) isPossible++;
+    if (arr[1] + arr[2] > arr[0]) isPossible++;
+    if (arr[2] + arr[0] > arr[1]) isPossible++;
 
-	return isPossible;
+    return isPossible;
 }
 
 double triangleArea( double arr[] ) {
-	double s = 0.0;
-	double area = 0.0;
+    double s = 0.0;
+    double area = 0.0;
 
-	s = (arr[0] + arr[1] + arr[2]) / 2;
-	area = sqrt(s * (s - arr[0]) * (s - arr[1]) * (s - arr[2]));
-	return area;
+    s = (arr[0] + arr[1] + arr[2]) / 2;
+    area = sqrt(s * (s - arr[0]) * (s - arr[1]) * (s - arr[2]));
+
+    return area;
 }
 
 /*
@@ -119,32 +125,33 @@ double triangleArea( double arr[] ) {
 @return 3 == "Equilateral Triangle"
 */
 int triangleType( double arr[] ) {
-	int type = 0;
+    int type = 0;
 
-	if (arr[0] == arr[1]) type++;
-	if (arr[1] == arr[2]) type++;
-	if (arr[2] == arr[0]) type++;
-	return type;
+    if (arr[0] == arr[1]) type++;
+    if (arr[1] == arr[2]) type++;
+    if (arr[2] == arr[0]) type++;
+
+    return type;
 }
 
 void printTriangleSides( double arr[] ) {
-	int i = 0;
+    int i = 0;
 
-	printf("(");
-	for (i = 0; i < 3; i++) {
-		printf("%.0lf", arr[i]);
-		if (i != 2) printf(", ");
-	}
-	printf(")");
+    printf("(");
+    for (i = 0; i < 3; i++) {
+        printf("%.2lf", arr[i]);
+        if (i != 2) printf(", ");
+    }
+    printf(")");
 }
 
 void printTriangleArea( double area ) {
-	printf("%.2lf", area);
+    printf("%.2lf", area);
 }
 
 void printTriangleType( int type ) {
-	if (type == 0) printf(" Scalene");
-	if (type == 1) printf(" Isosceles");
-	if (type == 3) printf("n Equilateral");
+    if (type == 0) printf(" Scalene");
+    if (type == 1) printf(" Isosceles");
+    if (type == 3) printf("n Equilateral");
 }
 
