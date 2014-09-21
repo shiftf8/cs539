3bd4013d570ce51d6e93e7cad45a0956b96ef995
Rob Lamog <shiftf8@gmail.com>
2014-09-21 00:37:20 -0700

    reorg files and start lab 2

---
+++ lab2aWords.c
@@ -0,0 +1,14 @@
+/*
+Lamog, Robert
+Lab 2A
+09/30/2014
+Stripping punctuation and blanks, putting a single word per line from formatted source input.
+*/
+
+#include <stdio.h>
+
+int main() {
+     
+
+    return 0; 
+} /* main */
