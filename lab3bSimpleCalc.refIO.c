1654c9dfa0f02e5a7f230d1de8b4975143a663ee
Rob <shiftf8@gmail.com>
2014-10-04 05:46:12 +0000

    Tried to refactor lab3 IO. It's proving very challenging. Not sure I can see a solution.

--- lab3bSimpleCalc.refIO.c
+++ lab3bSimpleCalc.refIO.c
@@ -7,71 +7,55 @@ Simple 2 unsigned int operand expression calculator using getchar() and putchar(
 
 #include <stdio.h>
 
-void simpleCalc();
-unsigned int makeOperand( char, unsigned int );
+void simpleExpression( char );
 int isNumChecker( char );
 int isOperator( char );
-void add(unsigned int, unsigned int);
-void subtract(unsigned int, unsigned int);
-void multiply(unsigned int, unsigned int);
-void divide(unsigned int, unsigned int);
-void modulo(unsigned int, unsigned int);
+unsigned int makeOperand( char, unsigned int );
+void add( unsigned int, unsigned int );
+void subtract( unsigned int, unsigned int );
+void multiply( unsigned int, unsigned int );
+void divide( unsigned int, unsigned int );
+void modulo( unsigned int, unsigned int );
 
 int main() {
     char response = 0;
 
     printf("Let's test some expressions.\n");
     while (response != '\n' || response != EOF) {
-        printf("Please enter an expression: ");
-        simpleCalc();
-        printf("Would you like to enter another expression (Y/N)?\n");
-        
         response = getchar();
-        if (response != 'n' || response != 'N') {
-            char c;
-            
-            while ((c = getchar()) != '\n' && c != EOF) {
-                /* Discarding the buffer stream. */
-            }
-        }
+        
         if (response == 'n' || response == 'N') break;
+        if (isNumChecker(response) || isOperator(response)) simpleExpression(response);
     }
     
     return 0;
 }
 
-void simpleCalc() {
+void simpleExpression( char c ) {
     unsigned int x = 0; /* Left operand. */
     unsigned int y = 0; /* Right operand. */
     int endOfNumFlag = 0;
     int operandSuccess = 0;
     int operationSuccess = 0;
-    char c = 0;
     char operation = 0;
 
-    while (c != EOF) {
-        c = getchar();
-        putchar(c);
-        
-        if (isNumChecker(c) == 1) {
-            if (operandSuccess == 0) x = makeOperand(c, x);
-            if (operandSuccess == 1) y = makeOperand(c, y);
-            endOfNumFlag = 1;
-        }
-        if (isNumChecker(c) == 0) {
-            if (endOfNumFlag == 1) {
-                operandSuccess++;
-                endOfNumFlag = 0;
-            }
-        }
-        if (isOperator(c)) {
-            if (operationSuccess == 0 && operandSuccess == 1) operation = c;
-            operationSuccess++;
-        }
-        if (c == '\n') break;
-    }
+    putchar(c);
     
-/*    printf("x = %d\ny = %d\nNum of Operands = %d\nOperation = %c\nNum of Operations = %d\n", x, y, operandSuccess, operation, operationSuccess);    */
+    if (isNumChecker(c) == 1) {
+        if (operandSuccess == 0) x = makeOperand(c, x);
+        if (operandSuccess == 1) y = makeOperand(c, y);
+        endOfNumFlag = 1;
+    }
+    if (isNumChecker(c) == 0) {
+        if (endOfNumFlag == 1) {
+            operandSuccess++;
+            endOfNumFlag = 0;
+        }
+    }
+    if (isOperator(c)) {
+        if (operationSuccess == 0 && operandSuccess == 1) operation = c;
+        operationSuccess++;
+    }
 
     if (operandSuccess == 2 && operationSuccess == 1) {
         if (operation == '+') add(x, y);
@@ -93,13 +77,13 @@ int isOperator( char c ) {
     if ((c == '+') || (c == '-') || (c == '*') || (c == '/') || (c == '%')) return 1;
     return 0;
 }
-void add(unsigned int a, unsigned int b) {
+void add( unsigned int a, unsigned int b ) {
     unsigned int total = 0;
     
     total = a + b;
     printf("%d + %d = %d\n", a, b, total);
 }
-void subtract(unsigned int a, unsigned int b) {
+void subtract( unsigned int a, unsigned int b ) {
     unsigned int total = 0;
     
     if (a >= b) {
@@ -107,13 +91,13 @@ void subtract(unsigned int a, unsigned int b) {
         printf("%d - %d = %d\n", a, b, total);
     } else printf("This calculator isn't capable of handling this expression.\n");
 }
-void multiply(unsigned int a, unsigned int b) {
+void multiply( unsigned int a, unsigned int b ) {
     unsigned int total = 0;
     
     total =  a * b;
     printf("%d * %d = %d\n", a, b, total);
 }
-void divide(unsigned int a, unsigned int b) {
+void divide( unsigned int a, unsigned int b ) {
     double total = 0.0;
     
     if (b == 0) printf("Undefined.\n");
@@ -122,7 +106,7 @@ void divide(unsigned int a, unsigned int b) {
         printf("%d / %d = %g\n", a, b, total);
     }
 }
-void modulo(unsigned int a, unsigned int b) {
+void modulo( unsigned int a, unsigned int b ) {
     unsigned int total = 0;
     
     total = a % b;
