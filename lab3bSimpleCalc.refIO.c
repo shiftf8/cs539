4a245be0566a1d949201e6e29380dce5cb2b5f78
Rob <shiftf8@gmail.com>
2014-10-05 02:01:03 +0000

    still working the refactor angle. interesting problem. still not working.

--- lab3bSimpleCalc.refIO.c
+++ lab3bSimpleCalc.refIO.c
@@ -7,7 +7,7 @@ Simple 2 unsigned int operand expression calculator using getchar() and putchar(
 
 #include <stdio.h>
 
-void simpleExpression( char );
+int buildExpression( char );
 int isNumChecker( char );
 int isOperator( char );
 unsigned int makeOperand( char, unsigned int );
@@ -18,23 +18,40 @@ void divide( unsigned int, unsigned int );
 void modulo( unsigned int, unsigned int );
 
 int main() {
+    /*  @return -1 == Processing.
+        @return 0 == Invalid expression.
+        @return 1 == Valid expression.
+    */
+    int expressionState = -1;
     char response = 0;
 
-    printf("Let's test some expressions.\n");
-    while (response != '\n' || response != EOF) {
-        response = getchar();
+    printf("Let's test some simple expressions.\nPlease input an expression: ");
+    while (response = getchar() != EOF) {
+        // response = getchar();
+        if (isNumChecker(response)) {
+            expressionState = buildExpression(response);
+        }
         
-        if (response == 'n' || response == 'N') break;
-        if (isNumChecker(response) || isOperator(response)) simpleExpression(response);
+        if (expressionState == 0) printf("Invalid expression.\nWould you like to input another expression (Y/N)?\n");
+        if (expressionState == 1) printf("Would you like to input another expression (Y/N)?\n");
+        if (response == 'N') {
+            printf("Thanks for playing. Goodbye.\n");
+            break;
+        }
+        if (response == 'Y') printf("Please input an expression: ");
     }
     
     return 0;
 }
 
-void simpleExpression( char c ) {
-    unsigned int x = 0; /* Left operand. */
-    unsigned int y = 0; /* Right operand. */
-    int endOfNumFlag = 0;
+/*  It would be easy to assign operand into arr[operandSuccess] to support more than two operands.
+    As well as, assigning operation into arr[operationSuccess].
+*/
+int buildExpression( char c ) {
+    unsigned int x = 0; /* First operand. */
+    unsigned int y = 0; /* Second operand. */
+    int operand = 0;
+    int endOfOperandFlag = 0;
     int operandSuccess = 0;
     int operationSuccess = 0;
     char operation = 0;
@@ -42,29 +59,46 @@ void simpleExpression( char c ) {
     putchar(c);
     
     if (isNumChecker(c) == 1) {
-        if (operandSuccess == 0) x = makeOperand(c, x);
-        if (operandSuccess == 1) y = makeOperand(c, y);
-        endOfNumFlag = 1;
+        operand = makeOperand(c, operand);
+        if (operandSuccess == 0) x = operand;
+        if (operandSuccess == 1) y = operand;
+        endOfOperandFlag = 1;
     }
     if (isNumChecker(c) == 0) {
-        if (endOfNumFlag == 1) {
+        if (endOfOperandFlag == 1) {
             operandSuccess++;
-            endOfNumFlag = 0;
+            endOfOperandFlag = 0;
         }
-    }
-    if (isOperator(c)) {
-        if (operationSuccess == 0 && operandSuccess == 1) operation = c;
-        operationSuccess++;
+        if (isOperator(c)) {
+            if (operandSuccess == 1 && operationSuccess == 0) operation = c;
+            operationSuccess++;
+        }
     }
 
     if (operandSuccess == 2 && operationSuccess == 1) {
-        if (operation == '+') add(x, y);
-        if (operation == '-') subtract(x, y);
-        if (operation == '*') multiply(x, y);
-        if (operation == '/') divide(x, y);
-        if (operation == '%') modulo(x, y);
-        if (operation == 0) printf("Invalid expression.\n");
-    } else printf("Invalid expression.\n");
+        if (operation == 0) return 0;
+        if (operation == '+') {
+            add(x, y);
+            return 1;
+        }
+        if (operation == '-') {
+            subtract(x, y);
+            return 1;
+        }
+        if (operation == '*') {
+            multiply(x, y);
+            return 1;
+        }
+        if (operation == '/') {
+            divide(x, y);
+            return 1;
+        }
+        if (operation == '%') {
+            modulo(x, y);
+            return 1;
+        }
+    }
+    return -1;
 }
 unsigned int makeOperand( char c, unsigned int operand ) {
     return (operand * 10) + (c - '0');
