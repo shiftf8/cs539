2cd8c936673b6767ed4d19a9298dcfa416cc879d
Rob <shiftf8@gmail.com>
2015-01-15 11:34:59 +0000

    one last function to write. strfilter()

--- lab4b.c
+++ lab4b.c
@@ -7,58 +7,63 @@ Simple letter replacement utilizing arrays.
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <time.h>
 
-int isUpperAlphaChecker( char[], unsigned int );
-int randomStr( char[] );
-void strfilters( char[], char[], char );
-void output( char[], char[], char, unsigned int );
+void randomString( char[], int );
+int isUpperAlpha( char );
+void strfilter( char[], char[], char );
 
 int main() {
     char s1[40] = "0";
     char s2[20] = "0";
-    char replace = '\0';
+    char c = '\0';
+    char userInput[50] = "0";
     unsigned int j = 0;
+
+    randomString(s1, 40);
+    // printf("%s\n", s1);
     
-    s1[0] = randomStr(s1);
-    
-    printf("Let's reset some letters. Please input at least two upper case letters to reset: ");
-    while (gets(s2) != EOF) {
-//        if (isUpperAlphaChecker(s2, sizeof))
+    printf("Let's reset some letters!\nPlease input at least 2 upper case letters to reset followed by a space and a \"reset\" character.\n");
+    while (fgets(userInput, 50, stdin) != NULL) {
+        if (userInput[0] == 'N' && userInput[1] == 'o') break;
+
+        // printf("%s", userInput);
+        while (isUpperAlpha(userInput[j]) && j < 20) {
+            s2[j] = userInput[j];
+            j++;
+        }
+        // printf("%s\n", s2);
+        c = userInput[j + 1];
+        // printf("%c / %d\n", c, j);
+        if (j < 2) printf("Invalid input.\nPlease input at least 2 upper case letters to reset followed by a space and a \"reset\" character.\n");
+        else {
+            printf("s1 = {\"%s\"}\n", randomStr);
+            printf("s2 = {\"%s\"}\n", userStr);
+            printf("c = {\"%c\"}\n", resetChar);
+            strfilter(s1, s2, c);
+        }
+        
+        c = '\0';
+        j = 0;
     }
 
     return 0;
 }
-int isUpperAlphaChecker( char userStr[], unsigned int user_response_size ) {
+
+void randomString( char str[], int strLength ) {
     unsigned int i = 0;
     
-    for (i; i < user_response_size; i++) {
-        if (!(userStr[i] >= 'A') && !(userStr[i] <= 'Z')) return 0;
-    }
-    return 1;
-}
-int randomStr( char s[] ) {
-    unsigned int i = 0;
+    srand(time(NULL));
     
-    for (i; i < 40; i++) {
-        s[i] = (rand() % 26) + 65;
+    for (i; i < strLength; i++) {
+        str[i] = (rand() % 26) + 65;
         // putchar(s[i]);
     }
-    return s[0];
 }
-void strfilters( char randomStr[], char userStr[], char replaceChar ) {
-    
+int isUpperAlpha(char c) {
+    if ((c >= 'A') && (c <= 'Z')) return 1;
+    return 0;
 }
-void output( char randomStr[], char userStr[], char replaceChar, unsigned int user_response_size ) {
-    unsigned i = 0, j = 0;
+void strfilter( char randStr[], char uStr[], char replaceChar ) {
     
-    for (i; i < 40; i++) {
-        randomStr[i];
-    }
-    for (j; j < user_response_size; j++) {
-        userStr[i];
-    }
-    printf("s1 = {\"%s\"}\n", randomStr);
-    printf("s2 = {\"%s\"}\n", userStr);
-    printf("c = {\"%c\"}\n", replaceChar);
 }
