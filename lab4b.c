c5af0bc03d1863af68a32f2256959debfa15d62b
Rob <shiftf8@gmail.com>
2015-01-18 10:46:17 +0000

    Almost there.

--- lab4b.c
+++ lab4b.c
@@ -9,22 +9,28 @@ Simple letter replacement utilizing arrays.
 #include <stdlib.h>
 #include <time.h>
 
-void randomString( char[], int );
+char randUpperChar();
 int isUpperAlpha( char );
+int isAlphaNum( char );
 void strfilter( char[], char[], char );
 
 int main() {
-    char s1[40] = "0";
-    char s2[20] = "0";
+    char s1[41] = "\0";
+    char s2[21] = "\0";
     char c = '\0';
-    char userInput[50] = "0";
+    char userInput[50] = "\0";
+    unsigned int i = 0;
     unsigned int j = 0;
+    unsigned int k = 0;
 
-    randomString(s1, 40);
+    srand(time(NULL)); //Generate random upper case array string
+    for (i; i < 40; i++) {
+        s1[i] = randUpperChar();
+    }
     // printf("%s\n", s1);
     
     printf("Let's reset some letters!\nPlease input at least 2 upper case letters to reset followed by a space and a \"reset\" character.\n");
-    while (fgets(userInput, 50, stdin) != NULL) {
+    while (fgets(userInput, 20, stdin) != NULL) {
         if (userInput[0] == 'N' && userInput[1] == 'o') break;
 
         // printf("%s", userInput);
@@ -35,35 +41,64 @@ int main() {
         // printf("%s\n", s2);
         c = userInput[j + 1];
         // printf("%c / %d\n", c, j);
-        if (j < 2) printf("Invalid input.\nPlease input at least 2 upper case letters to reset followed by a space and a \"reset\" character.\n");
+        if (j < 2 || !isAlphaNum(c)) printf("Invalid input.\nPlease input at least 2 upper case letters to reset followed by a space and a \"reset\" character.\n");
         else {
-            printf("s1 = {\"%s\"}\n", randomStr);
-            printf("s2 = {\"%s\"}\n", userStr);
-            printf("c = {\"%c\"}\n", resetChar);
+            printf("s1 = {\"%s\"}\n", s1);
+            printf("s2 = {\"%s\"}\n", s2);
+            printf("c = {\"%c\"}\n", c);
             strfilter(s1, s2, c);
         }
         
+        for (k; k < 20; k++) s2[k] = '\0';
         c = '\0';
         j = 0;
+        k = 0;
     }
 
     return 0;
 }
 
-void randomString( char str[], int strLength ) {
-    unsigned int i = 0;
-    
-    srand(time(NULL));
-    
-    for (i; i < strLength; i++) {
-        str[i] = (rand() % 26) + 65;
-        // putchar(s[i]);
+char randUpperChar() {
+    char alpha = '0';
+
+    while (!isUpperAlpha(alpha)) {
+        alpha = (rand() % 26) + 65;
     }
+    return alpha;
 }
-int isUpperAlpha(char c) {
+int isUpperAlpha( char c ) {
     if ((c >= 'A') && (c <= 'Z')) return 1;
     return 0;
 }
-void strfilter( char randStr[], char uStr[], char replaceChar ) {
+int isAlphaNum( char c ){
+    if ((c >= '!') && (c <= '~')) return 1;
+    return 0;
+}
+void strfilter( char randStr[], char uStr[], char resetChar ) {
+    char filteredStr[40] = "\0";
+    unsigned int i = 0;
+    unsigned int j = 0;
+    unsigned int k = 0;
+    unsigned int arrLength = 0;
     
+    while (isUpperAlpha(uStr[arrLength])) {
+        arrLength++;
+    }
+    // printf("%d\n", arrLength);
+
+    for (i; i < 40; i++) {
+        filteredStr[i] = randStr[i];
+        
+        for (j; j < arrLength; j++) {
+            if (randStr[i] == uStr[j]) filteredStr[i] = resetChar;
+        }
+    }
+    
+    printf("filtered s1 = {\"");
+    for (k; k < 40; k++) {
+        printf("%c", filteredStr[k]);
+    }
+    printf("\"}\n");
+    printf("Please input at least 2 upper case letters to reset followed by a space and a \"reset\" character.\n");
+    printf("Or type \"No\" to quit.\n");
 }
