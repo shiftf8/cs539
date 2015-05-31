aa905fc4d4f0e0900a478a9b7bcd7cf755d2becc
Rob <shiftf8@gmail.com>
2015-05-31 07:22:21 +0000

    Refactored for simplicity. Still isn't sorting correctly. Trying to implement Selection Sort.

--- lab6A.c
+++ lab6A.c
@@ -8,13 +8,13 @@ List words from a paragraph alphabetically. Disregard duplicates.
 #include <stdio.h>
 
 struct word {
-    char letters[100]; /* not accounting for words longer than 100 chars obviously */
+    char letters[50]; /* not accounting for longer words. */
 } word;
 
 int is_alpha_num_checker( char );
 int word_sort( struct word *, unsigned int );
-int ascii_alphabetical_check( struct word *, unsigned int, struct word *, unsigned int );
-void swap_words( struct word *, unsigned int, struct word *, unsigned int );
+int ascii_alphabetical_check( struct word *, struct word * );
+void swap_words( struct word *, struct word * );
 
 int main() {
     FILE *ipsumFile = NULL;
@@ -23,9 +23,10 @@ int main() {
     char c = '\0';
     unsigned int i = 0;
     unsigned int j = 0;
+/*   unsigned int k = 0; /* printf test var */
 
-    ipsumFile = fopen("input.stdloremipsum.txt", "r");
-    // ipsumFile = fopen("testinput.lab6a.txt", "r");
+/**/    ipsumFile = fopen("input.stdloremipsum.txt", "r"); /**/
+/*    ipsumFile = fopen("testinput.lab6a.txt", "r"); /* alternate test input */
 
     if (ipsumFile == NULL) perror("Error opening file.");
     else {
@@ -41,41 +42,40 @@ int main() {
                 if (isEndOfWord == 1) {
                     arrWords[i].letters[j] = '\0';
                     isEndOfWord = 0;
-                    j = 0;
                     i++;
+                    j = 0;
                 }
             }
             if (c == '\n') break;
         }
         fclose(ipsumFile);
-
-        if (!word_sort(&arrWords[0], i)) printf("alpha_sort: Unsuccessful!\n");
+    
+/*        for (k; k < i; k++) {
+            printf("%s\n", arrWords[k].letters);
+        } /* test printf */
+        
+        if (!word_sort(&arrWords[0], i)) printf("word_sort: Unsuccessful!\n");
     }
     
     return 0;
 } /* main */
 
 int is_alpha_num_checker( char c ) {
-    if (((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <='z')) || ((c >='0') && (c <= '9'))) return 1;
+    if (((c >='0') && (c <= '9')) || ((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <='z'))) return 1;
     return 0;
 }
 int word_sort( struct word *arrWords, unsigned int sizeOfarrWords ) {
     unsigned int i = 0;
     unsigned int j = 0;
     unsigned int min = 0;
-    unsigned int sizeOfWordX = 0;
-    unsigned int sizeOfWordMin = 0;
-    
+
     for (i; i < sizeOfarrWords; i++) {
-        while (arrWords[i].letters[sizeOfWordX] != '\0') sizeOfWordX++;
-        while (arrWords[min].letters[sizeOfWordMin] != '\0') sizeOfWordMin++;
+        min = i;
 
         for (j = i + 1; j < sizeOfarrWords; j++) {
-            if (ascii_alphabetical_check((&arrWords[j]), sizeOfWordX, (&arrWords[min]), sizeOfWordMin)) min = j;
+            if (ascii_alphabetical_check((&arrWords[j]), (&arrWords[min]))) min = j;
         }
-        swap_words(&arrWords[i], sizeOfWordX, &arrWords[min], sizeOfWordMin);
-        sizeOfWordX = 0;
-        sizeOfWordMin = 0;
+        swap_words(&arrWords[i], &arrWords[min]);
     }
     
     i = 0;
@@ -85,40 +85,22 @@ int word_sort( struct word *arrWords, unsigned int sizeOfarrWords ) {
     
     return 1;
 }
-int ascii_alphabetical_check( struct word *wordX, unsigned int sizeOfWordX, struct word *wordMin, unsigned int sizeOfWordMin ) {
+int ascii_alphabetical_check( struct word *wordX, struct word *wordMin ) {
     unsigned int i = 0;
     
-    if (sizeOfWordMin >= sizeOfWordX) {
-        for (i; i < sizeOfWordMin; i++) {
-            if (wordX->letters[i] != wordMin->letters[i]) {
-                if (wordX->letters[i] < wordMin->letters[i]) return 1;
-            }
-        }
-    } else {
-        for (i; i < sizeOfWordX; i++) {
-            if (wordX->letters[i] != wordMin->letters[i]) {
-                if (wordX->letters[i] < wordMin->letters[i]) return 1;
-            }
-        }
+    for (i; i < 50; i++) {
+        if (wordX->letters[i] > wordMin->letters[i]) return 1;
     }
-    
+
     return 0;
 }
-void swap_words( struct word *wordX, unsigned int sizeOfWordX, struct word *wordMin, unsigned int sizeOfWordMin ) {
+void swap_words( struct word *wordX, struct word *wordMin ) {
     unsigned int i = 0;
     char tmpC = '\0';
     
-    if (sizeOfWordMin >= sizeOfWordX) {
-        for (i; i < sizeOfWordMin; i++) {
-            tmpC = wordX->letters[i];
-            wordX->letters[i] = wordMin->letters[i];
-            wordMin->letters[i] = tmpC;
-        }
-    } else {
-        for (i; i < sizeOfWordX; i++) {
-            tmpC = wordX->letters[i];
-            wordX->letters[i] = wordMin->letters[i];
-            wordMin->letters[i] = tmpC;
-        }
+    for (i; i < 50; i++) {
+        tmpC = wordX->letters[i];
+        wordX->letters[i] = wordMin->letters[i];
+        wordMin->letters[i] = tmpC;
     }
 }
