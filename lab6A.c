5c854fa5e3c8067c216fae36e84afb160ea98c15
Rob <shiftf8@gmail.com>
2015-06-14 17:39:29 +0000

    tolower and duplicates eliminated. can't figure out end of array comparison yet, so i'm losing one result.

--- lab6A.c
+++ lab6A.c
@@ -5,9 +5,9 @@ Lab 6A
 Dynamically create a list of words from a paragraph alphabetically utilizing pointers to structs. Disregard duplicate words.
 */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 typedef struct Word {
     char letters[100]; /* not accounting for longer words. */
@@ -18,23 +18,24 @@ int is_alpha_num_checker( char );
 int word_sort( word **, unsigned int );
 int ascii_alphabetical_check( word *, word * );
 void swap_words( word *, word * );
+int is_next_duplicate( word *, word * );
 
 int main() {
     FILE *ipsumFile = NULL;
-    word *arrWords[1024]; /* not accounting for particularly lengthy paragraphs. */
+    word *arrWords[1024]; /* not accounting for particularly lengthy paragraphs. not as dynamic as i would like but works for now. */
     unsigned int isEndOfWord = 0;
     char c = '\0';
     unsigned int i = 0;
     unsigned int j = 0;
     unsigned int k = 0;
 
-/**/    ipsumFile = fopen("input.stdloremipsum.txt", "r"); /**/
-/*    ipsumFile = fopen("testinput.lab6a.txt", "r"); /* alternate test input */
+/*    ipsumFile = fopen("input.stdloremipsum.txt", "r"); /**/
+/**/    ipsumFile = fopen("testinput.lab6a.txt", "r"); /* alternate test input */
 
     if (ipsumFile == NULL) perror("Error opening file.");
     else {
         while (c != feof(ipsumFile) || c != EOF) {
-            c = fgetc(ipsumFile);
+            c = tolower(fgetc(ipsumFile));
 
             if (is_alpha_num_checker(c) == 1) {
                 if (isEndOfWord == 0) {
@@ -54,6 +55,10 @@ int main() {
                     isEndOfWord = 0;
                     i++;
                     j = 0;
+                    if (i > 1024) {
+                        printf("You've written an exceptionally long paragraph which this program was not adequately designed to handle.\nProgram terminated!\n");
+                        exit(EXIT_FAILURE);
+                    }
                 }
             }
             if (c == '\n') break;
@@ -63,7 +68,11 @@ int main() {
         if (!word_sort(&arrWords[0], i)) printf("word_sort: Unsuccessful!\n");
         else {
             for (k; k < i; k++) {
-                printf("%s\n", arrWords[k]->letters);
+                if (k < i - 1) {
+                    if (!is_next_duplicate(arrWords[k], arrWords[k + 1])) {
+                        printf("%s\n", arrWords[k]->letters);
+                    }
+                }
                 free(arrWords[k]);
                 arrWords[k] = NULL;
             }
@@ -80,25 +89,25 @@ int is_alpha_num_checker( char c ) {
 int word_sort( word **arrWords, unsigned int sizeOfarrWords ) {
     unsigned int i = 0;
     unsigned int j = 0;
-    unsigned int min = 0;
+    unsigned int max = 0;
 
     for (i; i < sizeOfarrWords; i++) {
-        min = i;
+        max = i;
         
         for (j = i; j < sizeOfarrWords; j++) {
-            if (ascii_alphabetical_check(arrWords[min], arrWords[j])) min = j;
+            if (ascii_alphabetical_check(arrWords[max], arrWords[j])) max = j;
         }
-        swap_words(arrWords[min], arrWords[i]);
+        swap_words(arrWords[max], arrWords[i]);
     }
     
     return 1;
 }
-int ascii_alphabetical_check( word *wordMin, word *wordX ) {
+int ascii_alphabetical_check( word *wordMax, word *wordX ) {
     unsigned int i = 0;
     
     for (i; i < 100; i++) {
-        if (wordMin->letters[i] < wordX->letters[i]) return 1;
-        if (wordMin->letters[i] > wordX->letters[i]) return 0;
+        if (wordMax->letters[i] > wordX->letters[i]) return 1;
+        if (wordMax->letters[i] < wordX->letters[i]) return 0;
     }
 
     return 0;
@@ -112,4 +121,12 @@ void swap_words( word *wordMin, word *wordX ) {
         wordX->letters[i] = wordMin->letters[i];
         wordMin->letters[i] = tmpC;
     }
+}
+int is_next_duplicate( word *wordX, word *wordNext ) {
+    unsigned int i = 0;
+    
+    for (i; i < 100; i++) {
+        if (wordX->letters[i] != wordNext->letters[i]) return 0;
+    }
+    return 1;
 }
