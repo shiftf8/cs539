a32d591b998928d6d6ed770f9b238d8da075c021
Rob Lamog <roblamog@yahoo.com>
2016-10-02 22:00:34 -0700

    Some scratch.c fun to test operators and precedence

--- scratch.c
+++ scratch.c
@@ -8,12 +8,26 @@ void foo(double*, double);
 int main(){
     double x;
     double y = 13;
+    double i2 = -2;
+    unsigned u1 = 1;
+    unsigned u2 = 2;
 
     printf("%p : %lf\n", &x, x);
+    printf("%p : %s\n", &"Blah", "Blah");
 
     foo(&x, y);
 
     printf("foo -> %p : %lf\n", &x, y);
+
+    printf("%lf\n", -i2++);
+
+    if (5 < 20 < 10) printf("WILD\n");
+
+//    u2 %= 2;
+//    printf("%u\n", u2);
+
+    u1 += u2 *= 3;
+    printf("%u / %u\n", u1, u2);
 
     return 0;
 }
