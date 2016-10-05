e31df6d753e7f42ad25a249320c192101d895980
Rob <shiftf8@gmail.com>
2016-10-05 15:49:55 +0000

    Start hw6pointers.c

---
+++ hw6pointers.c
@@ -0,0 +1,19 @@
+// CS 539, HW 6, Rob Lamog
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void show( const double * begin, const double * end );
+double sum( const double * begin, const double * end );
+void half( double * begin, double * end );
+void add( double * resultBegin, double * resultEnd, const double *a0Begin, const double * a1Begin );
+void maximum( double * resultBegin, double * resultEnd, const double *a0Begin, const double * a1Begin );
+void stat( double * smallest, double * average, double * largest, const double * begin, const double * end );
+void setFib( unsigned * begin, unsigned * end );
+void reverse( double * begin, double * end );
+int die( const char * msg );
+
+int main(){
+    
+    return 0;
+}
