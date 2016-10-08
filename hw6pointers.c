640e205225418bad8b2cb21e88ae9696411b83dd
Rob <shiftf8@gmail.com>
2016-10-08 05:55:18 +0000

    Update: created custom fillArray function. Pointer refactor progressing smoothly.

--- hw6pointers.c
+++ hw6pointers.c
@@ -13,32 +13,44 @@ void setFib( unsigned * begin, unsigned * end );
 void reverse( double * begin, double * end );
 int die( const char * msg );
 
+void fillArray( double * begin, double * end );
 int main(){
-    double *arr;
-    double *arr_head;
-    unsigned arr_length = 3;
-    double i = 0;
+    double *arr, *arr1, *arr2;
+    unsigned arr_length = 5;
 
     arr = malloc(arr_length * sizeof *arr);
     if (arr == NULL) die("malloc unsuccessful.");
-    printf("%p : %lf : %lu\n", &arr, *arr, sizeof *arr);
+    // printf("%p : %lf : %lu\n", &arr, *arr, sizeof *arr);
+    arr1 = malloc(arr_length * sizeof *arr1);
+    if (arr1 == NULL) die("malloc unsuccessful.");
+    arr2 = malloc(arr_length * sizeof *arr2);
+    if (arr2 == NULL) die("malloc unsuccessful.");
 
-    arr_head = arr; //Important to utilize separate pointer iterator instead of trying to constantly remember where original pointer came from.
-    // printf("%p / %p\n", &arr_head, &arr + arr_length);
-    while (arr_head < arr + arr_length){
-        *arr_head = i++;
-        ++arr_head;
-    }
+    fillArray(arr, arr + arr_length);
+    fillArray(arr1, arr1 + arr_length);
+    fillArray(arr2, arr2 + arr_length);
 
     show(arr, arr + arr_length);
+    show(arr1, arr1 + arr_length);
+    show(arr2, arr2 + arr_length);
     
     printf("sum = %lf\n", sum(arr, arr + arr_length));
     
-    arr_head = NULL; //Effectively "free" arr_head to prevent dangling pointer problem.
+    printf("half = ");
+    half(arr, arr + arr_length);
+    show(arr, arr + arr_length);
     
-    printf("%p : %lf : %lu\n", &arr, *arr, sizeof *arr);
+    printf("add = ");
+    add(arr, arr + arr_length, arr1, arr2);
+    show(arr, arr + arr_length);
+    
+    // printf("%p : %lf : %lu\n", &arr, *arr, sizeof *arr);
     free(arr);
     arr = NULL;
+    free(arr1);
+    arr1 = NULL;
+    free(arr2);
+    arr2 = NULL;
     
     return 0;
 }
@@ -55,16 +67,27 @@ void show( const double * begin, const double * end ){
 double sum( const double * begin, const double * end ){
     double retVal = 0;
     
-    while (end > begin++){
+    while (begin < end){
         retVal += *begin;
+        ++begin;
     }
     return retVal;
 }
 void half( double * begin, double * end ){
-    
+    while (begin < end){
+        *begin /= 2;
+        ++begin;
+    }
 }
 void add( double * resultBegin, double * resultEnd, const double *a0Begin, const double * a1Begin ){
-    
+    double *arr_head = resultBegin;
+    unsigned i = 0;
+
+    while (arr_head < resultEnd){
+        *arr_head = *(a0Begin + i) + *(a1Begin + i);
+        ++i;
+        ++arr_head;
+    }
 }
 void maximum( double * resultBegin, double * resultEnd, const double *a0Begin, const double * a1Begin ){
     
@@ -81,4 +104,14 @@ void reverse( double * begin, double * end ){
 int die( const char * msg ){
     printf("Fatal error: %s\n", msg);
     exit(EXIT_FAILURE);
+}
+
+void fillArray( double * begin, double * end ){
+    double *arr_head = begin;
+    unsigned i = 0;
+    
+    while (arr_head < end){
+        *arr_head = i++;
+        ++arr_head;
+    }
 }
