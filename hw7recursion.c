d8c80a97de6bbec2366b3f2c5b298a132004f167
Rob <shiftf8@gmail.com>
2016-10-26 04:00:47 +0000

    Update: First two recursions down.

--- hw7recursion.c
+++ hw7recursion.c
@@ -3,12 +3,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void countdown( unsigned seconds );
+unsigned add( unsigned a, unsigned b );
+unsigned multiply( unsigned a, unsigned b );
+
 int die( const char * msg );
 
 int main(){
+    countdown(5);
+    printf("\n");
+    
+    printf("%u\n", add(3, 7));
+    printf("\n");
+    
+    printf("%u\n", multiply(3, 7));
+    printf("\n");
     
     return 0;
 } //main
+
+void countdown( unsigned seconds ){
+    if (seconds > 0){
+        printf("%u...\n", seconds--);
+        countdown(seconds);
+    } else
+        printf("Blast off!\n");
+}
+unsigned add( unsigned a, unsigned b ){
+    if (a > 0){
+        add(--a, ++b);
+    } else
+        return b;
+}
+unsigned multiply( unsigned a, unsigned b ){
+    static unsigned product = 0;
+    
+    // if (a > 0){
+    //     product = add(a, b);
+    //     multiply(--a,)
+    // }
+    return 0;
+}
 
 int die( const char * msg ){
     printf("Fatal error: %s\n", msg);
