d3e76e2ce172bf0a6d9736ad989f64e543c3e5c1
Rob <shiftf8@gmail.com>
2016-12-16 06:13:53 +0000

    Breakthrough on file separation.

--- spring2016/lab6C.c
+++ spring2016/lab6C.c
@@ -1,45 +1,12 @@
-/*
-Lamog, Robert
-Lab 6C
-06/02/2016
-Dynamically input names, id numbers and interest codes (0-50), and output based on student id in ascending order pairing roommate id with same/similar interest code.
-*/
+// Lab6c new start
 
 #include "roommate.h"
-#include <stdio.h>
 
-int main() {
-    FILE* input = NULL;
-    struct RoommateInfo* studentList[50];
-    unsigned int studentX = 0;
-    unsigned int lineN = 0;
-    char str[512];
+int main(){
+    struct RoommateInfo* x;
     
-    input = fopen( "input.lab6C.txt", "r" );
-    
-    if ( input != NULL ) {
-        studentList[studentX] = new_student(); /* Try to add initial student */
-        if ( studentList[studentX] == NULL ) {
-            printf( "newAddress: Unsuccessful.\nProgram terminated.\n" );
-            exit( EXIT_FAILURE );
-        }
-            
-        while ( fgets( str, sizeof( str ), input ) ) {
-            
-            if ( lineN == 0 ) set_student_name( studentList[studentX], str );
-            
-            ++lineN;
-            break;
-        }
-        
-    } else {
-        perror( "fopen: Unsuccessful.\nProgram terminated.\n" );
-        exit( EXIT_FAILURE );
-    }
-    
-    fclose( input );
-    
-    print_roommate_info( studentList[studentX] );
+    x = set_student_name(new_student(), "Rob Lamog");
+    print_roommate_info(x);
     
     return 0;
 }
