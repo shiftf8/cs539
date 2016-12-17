e4447aab4042e2c23d69d21c288781c351c0a508
Rob <shiftf8@gmail.com>
2016-12-17 08:27:44 +0000

    Revert "Breakthrough on file separation."
    This reverts commit d3e76e2ce172bf0a6d9736ad989f64e543c3e5c1.

--- spring2016/lab6C.c
+++ spring2016/lab6C.c
@@ -1,12 +1,45 @@
-// Lab6c new start
+/*
+Lamog, Robert
+Lab 6C
+06/02/2016
+Dynamically input names, id numbers and interest codes (0-50), and output based on student id in ascending order pairing roommate id with same/similar interest code.
+*/
 
 #include "roommate.h"
+#include <stdio.h>
 
-int main(){
-    struct RoommateInfo* x;
+int main() {
+    FILE* input = NULL;
+    struct RoommateInfo* studentList[50];
+    unsigned int studentX = 0;
+    unsigned int lineN = 0;
+    char str[512];
     
-    x = set_student_name(new_student(), "Rob Lamog");
-    print_roommate_info(x);
+    input = fopen( "input.lab6C.txt", "r" );
+    
+    if ( input != NULL ) {
+        studentList[studentX] = new_student(); /* Try to add initial student */
+        if ( studentList[studentX] == NULL ) {
+            printf( "newAddress: Unsuccessful.\nProgram terminated.\n" );
+            exit( EXIT_FAILURE );
+        }
+            
+        while ( fgets( str, sizeof( str ), input ) ) {
+            
+            if ( lineN == 0 ) set_student_name( studentList[studentX], str );
+            
+            ++lineN;
+            break;
+        }
+        
+    } else {
+        perror( "fopen: Unsuccessful.\nProgram terminated.\n" );
+        exit( EXIT_FAILURE );
+    }
+    
+    fclose( input );
+    
+    print_roommate_info( studentList[studentX] );
     
     return 0;
 }
--- spring2016/roommate.h
+++ spring2016/roommate.h
@@ -10,7 +10,6 @@ For use with lab6C. Simple Student data structure based on predetermined file in
 #ifndef ROOMMATE_RECORD_H
 #define ROOMMATE_RECORD_H
 
-#include "roommate.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,11 +21,38 @@ typedef struct RoommateInfo {
     struct RoommateInfo* roommate;
 } RoommateInfo;
 
-struct RoommateInfo* new_student();
-void del_student( struct RoommateInfo* student );
-struct RoommateInfo* set_student_name( struct RoommateInfo* student, const char* str );
-struct RoommateInfo* set_id( struct RoommateInfo* student, const unsigned int id );
-struct RoommateInfo* set_interest_code( struct RoommateInfo* student, const unsigned int interestCode );
-void print_roommate_info( const struct RoommateInfo* info );
+struct RoommateInfo* new_student() {
+    struct RoommateInfo* retVal = malloc( sizeof( struct RoommateInfo ) );
+    if ( retVal == NULL ) return NULL;
+
+    return retVal;
+}
+void del_student( struct RoommateInfo* student ) {
+    if ( student != NULL ) free( student );
+}
+
+struct RoommateInfo* set_student_name( struct RoommateInfo* student, char* str ) {
+    strcpy( student->studentName, str );
+
+    return student;
+}
+struct RoommateInfo* set_id( struct RoommateInfo* student, unsigned int id ) {
+    student->studentID = id;
+
+    return student;
+}
+struct RoommateInfo* set_interest_code( struct RoommateInfo* student, unsigned int interestCode ) {
+    student->interestCode = interestCode;
+    
+    return student;
+}
+
+void print_roommate_info( struct RoommateInfo* info ) {
+    /* Test print code */
+    printf( "Student: %s\nID: %d\nInterest Code: %d\n", info->studentName, info->studentID, info->interestCode );
+    printf( "Roomate: %s\n", info->roommate->studentName ); /**/
+    
+    /* printf( "Student: %s\nRoommate: %s\n", info->studentName, info->roommate->studentName ); /**/
+}
 
 #endif /* ROOMMATE_RECORD_H */
