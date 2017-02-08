4152947c420d82adcbb43a4d07dba316a6f650fb
Rob <shiftf8@gmail.com>
2017-02-08 04:17:57 +0000

    First lab. lab1b.c. Looks like the same workbook as before. I can refactor my old code.

---
+++ lab1b.c
@@ -0,0 +1,14 @@
+/*
+Lamog, Robert
+Lab 1B
+02/21/2017
+Testing sides to make equilateral, isosceles, or scalene triangles.
+*/
+
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
