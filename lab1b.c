2862aca60f9437b96e648f35476e1bd5a43e9762
Rob <shiftf8@gmail.com>
2017-02-16 20:47:55 +0000

    Update: Minor UX edits. Added print statements for printing user input.

--- lab1b.c
+++ lab1b.c
@@ -16,12 +16,13 @@ int main() {
     int inputChar, loopAgain;
 
     do {
-        printf("Enter 3 sides of a triangle (a b c): ");
+        printf("Enter 3 lengths to test for a triangle (a b c): ");
         test_input_triangle();
-        printf("Would you like to enter three new sides (Y/N)? ");
+        printf("Would you like to enter three new lengths (Y/N)? ");
         inputChar = getchar();
         if (inputChar == 'Y' || inputChar == 'y') loopAgain = 1;
         else loopAgain = 0;
+        printf("%c\n", inputChar);
     } while (loopAgain);
 
     return 0;
@@ -30,17 +31,17 @@ int main() {
     Return 1 if the three inputs can make a valid triangle.
     Return 0 if NOT a valid triangle.
 */
-int is_valid_triangle(unsigned a, unsigned b, unsigned c){
-    if (a + b > c
-        && b + c > a
-        && c + a > b) return 1;
+int is_valid_triangle(unsigned s1, unsigned s2, unsigned s3){
+    if (s1 + s2 > s3
+        && s2 + s3 > s1
+        && s3 + s1 > s2) return 1;
     return 0;
 }
-double area_of_triangle(unsigned a, unsigned b, unsigned c){
+double area_of_triangle(unsigned s1, unsigned s2, unsigned s3){
     double semiPerimeter, areaOfTriangle;
 
-    semiPerimeter = (a + b + c) / 2.0;
-    areaOfTriangle = sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+    semiPerimeter = (s1 + s2 + s3) / 2.0;
+    areaOfTriangle = sqrt(semiPerimeter * (semiPerimeter - s1) * (semiPerimeter - s2) * (semiPerimeter - s3));
 
     return areaOfTriangle;
 }
@@ -49,14 +50,15 @@ void test_input_triangle(){
     int isNewLine;
 
     if (scanf("%u %u %u", &side1, &side2, &side3) == 3){
+        printf("%d %d %d\n", side1, side2, side3);
         if (is_valid_triangle(side1, side2, side3)){
-            printf("Sides %u, %u, and %u make a", side1, side2, side3);
-            if (side1 == side2 && side1 == side3) printf("n equilateral ");
-            else if (side1 == side2 || side2 == side3 || side3 == side1) printf("n isosceles ");
-            else printf(" scalene ");
-            printf("triangle with an area of %f\n", area_of_triangle(side1, side2, side3));
+            printf("%u, %u, and %u make a", side1, side2, side3);
+            if (side1 == side2 && side1 == side3) printf("n EQUILATERAL ");
+            else if (side1 == side2 || side2 == side3 || side3 == side1) printf("n ISOSCELES ");
+            else printf(" SCALENE ");
+            printf("triangle with an approximate area of %f.\n", area_of_triangle(side1, side2, side3));
         } else {
-            printf("Sides %u, %u, and %u do NOT seem to constitute a valid triangle.\n", side1, side2, side3);
+            printf("%d, %d, and %d do NOT seem to constitute a valid triangle.\n", side1, side2, side3);
         }
     } else printf("Invalid entry.\n");
 
