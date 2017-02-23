1f5026a92b9edae999aeca2ac9ba6882e5d61330
Rob <shiftf8@gmail.com>
2017-02-23 02:06:54 +0000

    Update: lab2a.c and input.lab2a.txt complete.

--- lab2a.c
+++ lab2a.c
@@ -8,9 +8,11 @@ Stripping punctuation and blanks, putting a single word per line from formatted 
 #include <stdio.h>
 
 /*
-    Built my own alpha checker.
+    Built my own simple alpha checker.
+    Return 1 if char is in range of 'a' to 'z' or 'A' to 'Z'.
+    Return 0 if char is NOT in range stated above.
 */
-int is_alpha_checker( char );
+int is_alpha( char );
 /*
     Reads input redirect via a loop utilizing getchar().
     If the character received is a letter, it prints the character via putchar().
@@ -18,28 +20,28 @@ int is_alpha_checker( char );
 */
 void word_per_line();
 
-int main() {
+int main(){
     word_per_line();
 
     return 0;
 }
 
-int is_alpha_checker( char inputChar ){
-    if (((inputChar >= 'A') && (inputChar <= 'Z')) || ((inputChar >= 'a') && (inputChar <='z'))) return 1;
+int is_alpha( char inputChar ){
+    if ((inputChar >= 'A' && inputChar <= 'Z') || (inputChar >= 'a' && inputChar <='z')) return 1;
     return 0;
 }
 void word_per_line(){
     int endOfWordFlag = 0;
     char c;
 
-    while (c != EOF) {
+    while (c != EOF){
         c = getchar();
-        if (is_alpha_checker(c) == 1) {
+        if (is_alpha(c) == 1){
             putchar(c);
             endOfWordFlag = 1;
         }
-        if (is_alpha_checker(c) == 0) {
-            if (endOfWordFlag == 1) {
+        if (is_alpha(c) == 0){
+            if (endOfWordFlag == 1){
                 printf("\n");
                 endOfWordFlag = 0;
             }
