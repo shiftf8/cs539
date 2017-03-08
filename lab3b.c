947a9cfcc09951343ab19caa873bc0300608cef1
Rob <shiftf8@gmail.com>
2017-03-08 17:56:10 +0000

    Update: Refactor of lab3b.c coming along nicely.

--- lab3b.c
+++ lab3b.c
@@ -2,79 +2,146 @@
 Lamog-Doiel, Robert
 Lab 3B
 3/21/2017
-Simple 2 unsigned int operand expression calculator using getchar() and putchar() for input/output of user input.
+Simple, 2 unsigned operand expression calculator using getchar() and putchar() for input/output of user input.
+Valid operations include '+', '-', '*', '/', '%'.
 */
 
 #include <stdio.h>
 
-void test_expression();
-int is_number(char);
-int is_valid_operator(char);
-unsigned make_operand(char, unsigned);
-void add(unsigned, unsigned);
-void subtract(unsigned, unsigned);
-void multiply(unsigned, unsigned);
-void divide(unsigned, unsigned);
-void modulo(unsigned, unsigned);
+/*
+    Uses getchar() and putchar() via loop to input/output user input.
+    Analyzes input for valid expression which includes unsigned int, valid operator, unsigned int. e.g. a + b
+    NOT white space dependent. e.g.     a%   b
+    Evaluates valid expression and prints it, else, print "Invalid expression.\n"
+*/
+void testExpression();
+/*
+    My own isSpace().
+    Return 1 if char is ' ', '\f', '\n', '\r', '\t', '\v'.
+    Return 0 if char is NOT valid.
+*/
+int isSpace(char);
+/*
+    My own isDigit().
+    Return 1 if char is in range of '0' to '9'.
+    Return 0 if char is NOT in range.
+*/
+int isDigit(char);
+/*
+    Checks char for valid operator.
+    Return 1 if char is '+', '-', '*', '/', '%'.
+    Return 0 if char is NOT valid.
+*/
+int isValidOperator(char);
+/*
+    Converts char to unsigned int digit and appends it to existing unsigned int.
+    Returns appended unsigned int.
+*/
+unsigned makeOperand(char, unsigned);
+/*
+    Returns a + b.
+*/
+unsigned add(unsigned, unsigned);
+/*
+    Returns a - b.
+    subtract() returns an int because it is possible to evaluate a valid expression to a negative number.
+*/
+int subtract(unsigned, unsigned);
+/*
+    Returns a * b.
+*/
+unsigned multiply(unsigned, unsigned);
+/*
+    Returns -1 if b == 0 to catch special case of "Undefined.\n"
+    Returns a / b.
+    divide() returns a double because it is possible to evaluate a valid expression to a fractional number.
+*/
+double divide(unsigned, unsigned);
+/*
+    Returns a % b.
+*/
+unsigned modulo(unsigned, unsigned);
 
 int main(){
-    int loopAgain;
-    char inputChar;
+    int loop_again;
+    char input_char;
 
-    // do {
-    //     printf("Enter a simple expression (a (+-*/%%) c): ");
-    //     test_expression();
-    //     printf("Would you like to enter a new expression (Y/N)? ");
-    //     inputChar = getchar();
-    //     if (inputChar == 'Y' || inputChar == 'y') loopAgain = 1;
-    //     else loopAgain = 0;
-    // } while (loopAgain);
-
-    divide(4, 2);
+    do {
+        printf("Enter a simple expression (a [+-*/%%] c): ");
+        testExpression();
+        printf("Would you like to enter a new expression (Y/N)? ");
+        input_char = getchar();
+        if (input_char == 'Y' || input_char == 'y'){
+            putchar(input_char);
+            loop_again = 1;
+        } else loop_again = 0;
+    } while (loop_again);
 
     return 0;
 }
 
-void test_expression(){
-    char inputChar, operation;
-    unsigned firstOperand, secondOperand;
-    unsigned numberOfOperands;
-    int isOperand;
+void testExpression(){
+    char input_char, operation;
+    unsigned first_operand = 0;
+    unsigned second_operand = 0;
+    unsigned number_of_operands = 0;
+    unsigned number_of_operations = 0;
+    int is_operand;
 
-    while ((inputChar = getchar()) != EOF){
-        putchar(inputChar);
+    while ((input_char = getchar()) != '\n' || input_char != EOF){
+        putchar(input_char);
 
-        if (is_number(inputChar)){
-            if (numberOfOperands == 0) make_operand(c, firstOperand);
-            if (numberOfOperands == 1) make_operand(c, secondOperand);
-            isOperand++;
+        if (!isSpace(input_char) && !isDigit(input_char) && !isValidOperator(input_char)){
+            printf("Invalid expression.\n");
+            break;
+        }
+        if (isDigit(input_char)){
+            if (number_of_operands == 0) first_operand = makeOperand(input_char, first_operand);
+            if (number_of_operands == 1) second_operand = makeOperand(input_char, second_operand);
+            is_operand = 1;
+        }
+        if (!isDigit(input_char) && is_operand == 1){
+            number_of_operands++;
+            is_operand = 0;
+        }
+        if (isValidOperator(input_char)){
+            if (number_of_operands == 1 && number_of_operations == 0) operation = input_char;
+            number_of_operations++;
         }
     }
+
+    if (number_of_operands == 2 && number_of_operations == 1){
+
+    } else printf("Invalid expression.\n");
 }
-int is_number(char c){
+int isSpace(char c){
+    if ((c == ' ') || (c == '\f') || (c == '\n') || (c == '\r') || (c == '\t') || (c == '\v')) return 1;
+    return 0;
+}
+int isDigit(char c){
     if ((c >= '0') && (c <= '9')) return 1;
     return 0;
 }
-int is_valid_operator(char c){
+int isValidOperator(char c){
     if ((c == '+') || (c == '-') || (c == '*') || (c == '/') || (c == '%')) return 1;
     return 0;
 }
-unsigned make_operand(char c, unsigned operand){
+unsigned makeOperand(char c, unsigned operand){
     return (operand * 10) + (c - '0');
 }
-void add(unsigned a, unsigned b){
-    printf(" = %d\n", (a + b));
+unsigned add(unsigned a, unsigned b){
+    return a + b;
 }
-void subtract(unsigned a, unsigned b){
-    printf(" = %d\n", (a - b));
+int subtract(unsigned a, unsigned b){
+    return a - b;
 }
-void multiply(unsigned a, unsigned b){
-    printf(" = %d\n", (a * b));
+unsigned multiply(unsigned a, unsigned b){
+    return a * b;
 }
-void divide(unsigned a, unsigned b){
-    if (b == 0) printf(" = Undefined\n");
-    else printf(" = %f\n", ((a * 1.0) / b));
+double divide(unsigned a, unsigned b){
+    if (b == 0) return -1;
+    return (a * 1.0) / b;
 }
-void modulo(unsigned a, unsigned b){
-    printf(" = %d\n", (a % b));
+unsigned modulo(unsigned a, unsigned b){
+    return a % b;
 }
