f52c7c4a01556ce4026f1dcace0689ada0cd945a
Rob <shiftf8@gmail.com>
2017-03-10 09:46:05 +0000

    Update: closer to cleaner lab3b.c but buffer streams are fun.

--- lab3b.c
+++ lab3b.c
@@ -12,15 +12,9 @@ Valid operations include '+', '-', '*', '/', '%'.
     Uses getchar() and putchar() via loop to input/output user input.
     Analyzes input for valid expression which includes unsigned int, valid operator, unsigned int. e.g. a + b
     NOT white space dependent. e.g.     a%   b
-    Evaluates valid expression and prints it, else, print "Invalid expression.\n"
+    Evaluates valid expression and prints solution, else, print "Invalid expression.\n"
 */
 void testExpression();
-/*
-    My own isSpace().
-    Return 1 if char is ' ', '\f', '\n', '\r', '\t', '\v'.
-    Return 0 if char is NOT valid.
-*/
-int isSpace(char);
 /*
     My own isDigit().
     Return 1 if char is in range of '0' to '9'.
@@ -63,60 +57,69 @@ double divide(unsigned, unsigned);
 unsigned modulo(unsigned, unsigned);
 
 int main(){
-    int loop_again;
-    char input_char;
+    int loop_again = 0;
+    char c = NULL;
 
     do {
         printf("Enter a simple expression (a [+-*/%%] c): ");
         testExpression();
         printf("Would you like to enter a new expression (Y/N)? ");
-        input_char = getchar();
-        if (input_char == 'Y' || input_char == 'y'){
-            putchar(input_char);
+        c = getchar();
+        if (c == 'Y' || c == 'y'){
+            putchar(c);
+            printf("\n");
             loop_again = 1;
-        } else loop_again = 0;
+        } else {
+            putchar(c);
+            printf("\n");
+            loop_again = 0;
+        }
     } while (loop_again);
 
     return 0;
 }
 
 void testExpression(){
-    char input_char, operation;
+    char c = NULL;
+    char operation = NULL;
     unsigned first_operand = 0;
     unsigned second_operand = 0;
     unsigned number_of_operands = 0;
     unsigned number_of_operations = 0;
-    int is_operand;
+    int is_operand = 0;
 
-    while ((input_char = getchar()) != '\n' || input_char != EOF){
-        putchar(input_char);
+    while ((c = getchar()) != EOF){
+        if (c != '\n') putchar(c);
 
-        if (!isSpace(input_char) && !isDigit(input_char) && !isValidOperator(input_char)){
-            printf("Invalid expression.\n");
-            break;
-        }
-        if (isDigit(input_char)){
-            if (number_of_operands == 0) first_operand = makeOperand(input_char, first_operand);
-            if (number_of_operands == 1) second_operand = makeOperand(input_char, second_operand);
+        if (isDigit(c)){
+            if (number_of_operands == 0) first_operand = makeOperand(c, first_operand);
+            if (number_of_operands == 1) second_operand = makeOperand(c, second_operand);
             is_operand = 1;
         }
-        if (!isDigit(input_char) && is_operand == 1){
+        if (!isDigit(c) && is_operand == 1){
             number_of_operands++;
             is_operand = 0;
         }
-        if (isValidOperator(input_char)){
-            if (number_of_operands == 1 && number_of_operations == 0) operation = input_char;
+        if (isValidOperator(c)){
             number_of_operations++;
+            operation = c;
+        }
+
+        if (c == '\n'){
+            if (number_of_operations == 0 || number_of_operations > 1) printf("Invalid expression.\n");
+            if (number_of_operands == 2 && number_of_operations == 1){
+                if (operation == '+') printf(" = %d\n", add(first_operand, second_operand));
+                if (operation == '-') printf(" = %d\n", subtract(first_operand, second_operand));
+                if (operation == '*') printf(" = %d\n", multiply(first_operand, second_operand));
+                if (operation == '/'){
+                    if (divide(first_operand, second_operand) == -1) printf(" = Undefined.\n");
+                    else printf(" = %f\n", divide(first_operand, second_operand));
+                }
+                if (operation == '%') printf(" = %d\n", modulo(first_operand, second_operand));
+            }
+            break;
         }
     }
-
-    if (number_of_operands == 2 && number_of_operations == 1){
-
-    } else printf("Invalid expression.\n");
-}
-int isSpace(char c){
-    if ((c == ' ') || (c == '\f') || (c == '\n') || (c == '\r') || (c == '\t') || (c == '\v')) return 1;
-    return 0;
 }
 int isDigit(char c){
     if ((c >= '0') && (c <= '9')) return 1;
