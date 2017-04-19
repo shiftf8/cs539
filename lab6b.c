1379aa864fb2af5ea298358025935aef28c1a780
Rob <shiftf8@gmail.com>
2017-04-19 10:16:57 +0000

    Final: Submit lab4b.c and lab5b.c. Started lab6b.c and lab6b.h

---
+++ lab6b.c
@@ -0,0 +1,11 @@
+/*
+Lamog, Robert
+Lab 6B
+05/09/2017
+Dynamically allocate array of pointers to structs from file input.
+Names and addresses are in alphabetical order.
+Output based on ascending zip code.
+Include duplicate entries.
+*/
+
+#include "lab6b.h"
---
+++ lab6b.h
@@ -0,0 +1,16 @@
+/*
+Lamog, Robert
+Lab 6B
+05/09/2017
+HEADER
+Dynamically allocate array of pointers to structs from file input.
+Names and addresses are in alphabetical order.
+Output based on ascending zip code.
+Include duplicate entries.
+*/
+
+
+#ifndef ADDRESS_RECORD_H
+#define ADDRESS_RECORD_H
+
+#endif
